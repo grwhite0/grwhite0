3c92be24123e6562c9497f75c9c1560645cb45ff
Gaby White <gwhite@mail.wlu.edu>
2025-09-15 09:46:55 -0400

    Create First.java

---
+++ First.java
@@ -0,0 +1,25 @@
+/**
+ * This class demonstrates some basic Java syntax.
+ * Note that this code is "overcommented".  I am trying to explain
+ * the code to you.  You should not need this many comments in your code.
+ * However, if the comments are helpful to you, you can continue with them.
+ *
+ * All code in a Java program must belong to a class.
+ * There is typically one class per file, and the file name is ClassName.java
+ * Compile this code using
+ *     javac First.java
+ * Run the compiled bytecode First.class using
+ *     java First
+ *
+ * @author Sara Sprenkle
+ */
+public class First {  // File must be saved as First.java
+  
+   /**
+    * Called when user runs 
+    *  java First
+    */
+   public static void main(String[] args) { // Blocks start/end with {}
+       System.out.println("This is my first Java program!"); // Statements end with ;
+   }
+}
